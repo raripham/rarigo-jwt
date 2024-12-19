f98fe3abea0717b3f3b62bc0c58c585b70e8ad4c
Phạm Thế Trọng <trongpt@ftech.ai>
2024-12-19 17:29:03 +0700

    update admin page

--- flushcache-cms/src/App.tsx
+++ flushcache-cms/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from './components/Login';
 import AdminPage from './components/AdminPage';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   const handleLogin = async (email: string) => {
     console.log('Email:', email);
+    setIsAuthenticated(true);
   };
 
   return (
@@ -13,7 +16,12 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginForm onSubmit={handleLogin}/>} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin" 
+          element={
+            isAuthenticated 
+              ? <AdminPage /> 
+              : <Navigate to="/login" replace />
+          } />
       </Routes>
     </Router>
     // <div className="App">
